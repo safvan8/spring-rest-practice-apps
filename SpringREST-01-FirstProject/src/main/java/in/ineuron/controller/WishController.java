272e5b8d56acdbe4fe41cd1e4a7799c5c5e80969
Safvan <safvanpmcz@gmail.com>
2023-05-19 08:57:57 +0530

    first commit

---
+++ SpringREST-01-FirstProject/src/main/java/in/ineuron/controller/WishController.java
@@ -0,0 +1,37 @@
+package in.ineuron.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/wish")
+public class WishController {
+
+	@GetMapping(value = "/msg")
+	@ResponseBody
+	public ResponseEntity<String> generateWishMessage() {
+
+		LocalDateTime ldt = LocalDateTime.now();
+		int hour = ldt.getHour();
+
+		String body = null;
+		if (hour < 12)
+			body = "Good Morning";
+		else if (hour < 16)
+			body = "Good Afternoon";
+		else if (hour < 20)
+			body = "Good Evening";
+		else
+			body = "Good night";
+
+		ResponseEntity<String> entity = new ResponseEntity<>(body, HttpStatus.OK);
+
+		return entity;
+	}
+}
